a3bcdf9ca4c8c670ea1db65a3d9ccadeaa77a26b
Mikael Eliasson <mikael@bokio.se>
2021-01-30 07:46:09 +0100

    Initial non-working state. Similar versions as we have in Bokio

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using WireMock.Server;
+using Xunit;
+
+namespace WireMockExperiment
+{
+    public class Class1
+    {
+        [Fact]
+        public async Task DummyTest()
+        {
+            var server = WireMockServer.Start(new WireMock.Settings.WireMockServerSettings
+            {
+                Port = 50100,
+                Logger = new WireMock.Logging.WireMockConsoleLogger(),
+                
+            });
+            server.Given(
+                    WireMock.RequestBuilders.Request.Create().WithPath("/test"))
+                .RespondWith(WireMock.ResponseBuilders.Response.Create().WithBody("Test"));
+
+            var client = new HttpClient();
+            var res = await client.GetStringAsync("http://localhost:50100/test");
+            Assert.Equal("Test", res);
+        }
+    }
+}
